3139fa7100c719ae6f85ed297849e13d76b38052
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-07 14:39:17 -0800

    YES WE GOT NTH STATES WORKING

--- source/markov_N.py
+++ source/markov_N.py
@@ -9,19 +9,11 @@ class MarkovNthOrder(object):
     def __init__(self, order=1):
         self.states = {}
 
-        if (len(sys.argv) > 1):
-            if order > 0:
-                self.order = sys.argv[1]
-                print("\nOrder defined as {}.".format(self.order))
-            else:
-                raise Exception("\nImproper parameter given: please give integer.")
+        if order > 0:
+            self.order = order
+            print("\nOrder defined as {}.".format(self.order))
         else:
-            if order > 0:
-                self.order = order
-                print("\nOrder defined as {}.".format(self.order))
-            else:
-                raise Exception("\nImproper parameter given: please give integer.")
-
+            raise Exception("\nImproper parameter given: please give integer.")
     
     def create_chain(self):
         n = self.order
@@ -32,6 +24,22 @@ class MarkovNthOrder(object):
 
         return tuple(chain_struct)
 
+    def build_sequence(self, old, step):
+        new = old[1:] + (step, )
+        return new
+
+    def build_states_from_sentence(self, input_sentence):
+        prev = self.create_chain()
+        tokens = input_sentence.split(" ")
+
+        for token in tokens:
+            if not prev in self.states:
+                self.states[prev] = []
+            curr = self.build_sequence(prev, token)
+            self.states[prev].append(curr)
+            prev = curr
+
+        print("input_sentence: {}\n\norder: {}\n\ntokens: {}\n\nstates: {}\n\n".format(input_sentence, self.order, tokens, self.states))
 
 
 # ================================================================================
@@ -39,10 +47,11 @@ class MarkovNthOrder(object):
 # ================================================================================
 
 def create_model():
-    separator = " "
-
     test_sentence = "will you participate in the conference with new fellows on saturday evening after registration will you participate in the workshops on monday morning interested in sharing a room"
-    markov = MarkovNthOrder()
+    
+    markov = MarkovNthOrder(3)
+    markov.build_states_from_sentence(test_sentence)
+
     return
 
 
